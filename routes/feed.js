f3cf463b5e402408f92ffb8333f77faa11db669b
Tomasz Duda <tduda@online.saba.com>
2021-06-30 16:50:19 +0200

    added validation and response

--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const {body} = require('express-validator')
 const feedController = require('../controllers/feed');
 const router = express.Router();
 
 
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
-router.post('/posts', feedController.postPost);
+router.post('/post', [
+    body('title').trim().isLength({
+        min: 5,
+        max: 20
+    }),
+    body("content").trim().isLength({
+        min: 5,
+        max: 300
+    })
+],feedController.postPost);
 
 module.exports = router;
